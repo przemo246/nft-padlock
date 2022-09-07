1aef07853e542850524a31ce2e27ac7f28ba1a03
spaceh3ad <jan.kwiatkowski.web3@gmail.com>
2022-09-07 16:48:49 +0200

    fixed tests

--- contracts/mocks/WETH.sol
+++ contracts/mocks/WETH.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
     constructor() ERC20("WETH Mock", "WETHM") {
-        _mint(msg.sender, 100000 * 10**18);
+        _mint(msg.sender, 100_000 * 10**18);
     }
 }
