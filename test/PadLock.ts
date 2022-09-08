b4585e032ea50b87689cf61b761b14984a4efde4
spaceh3ad <jan.kwiatkowski.web3@gmail.com>
2022-09-08 17:45:14 +0200

    added tests + burn

--- test/PadLock.ts
+++ test/PadLock.ts
@@ -7,6 +7,8 @@ import {
   PadLock,
   WETH,
   WETH__factory,
+  ERC1155NFT__factory,
+  ERC1155NFT,
 } from "../typechain-types";
 
 describe("Padlock", function () {
@@ -16,6 +18,7 @@ describe("Padlock", function () {
   let executor: SignerWithAddress;
   let weth: WETH;
   let padlock: PadLock;
+  let erc1155: ERC1155NFT;
 
   beforeEach(async () => {
     [deployer, bob, alice, executor] = await ethers.getSigners();
@@ -24,6 +27,10 @@ describe("Padlock", function () {
     padlock = await new PadLock__factory(deployer).deploy(
       executor.address,
       weth.address
+    );
+
+    erc1155 = await new ERC1155NFT__factory(deployer).attach(
+      await padlock.erc1155()
     );
 
     await weth.transfer(alice.address, ethers.utils.parseEther("1"));
@@ -55,6 +62,31 @@ describe("Padlock", function () {
     );
   });
 
+  it("Should allow to submitBreakUp", async () => {
+    const relationshipId = await submitRelationship();
+    await padlock.connect(alice).approveRelationship(relationshipId);
+
+    await erc1155.connect(alice).setApprovalForAll(padlock.address, true);
+
+    expect(await padlock.connect(alice).submitBreakUp()).to.emit(
+      padlock,
+      "BreakupProposal"
+    );
+  });
+
+  it("Should allow to approve submitBreakUp", async () => {
+    const relationshipId = await submitRelationship();
+    await padlock.connect(alice).approveRelationship(relationshipId);
+
+    await erc1155.connect(alice).setApprovalForAll(padlock.address, true);
+    await erc1155.connect(bob).setApprovalForAll(padlock.address, true);
+
+    await padlock.connect(alice).submitBreakUp();
+    expect(await padlock.connect(bob).approveBreakUp())
+      .to.emit(padlock, "BreakUp")
+      .withArgs(relationshipId, alice.address, bob.address);
+  });
+
   async function submitRelationship() {
     const tx = await padlock.connect(bob).submitRelationship(alice.address);
     const waitedTx = await tx.wait();
@@ -62,7 +94,7 @@ describe("Padlock", function () {
     const event = waitedTx?.events?.find(
       (event) => event.event === "RelationshipSubmitted"
     );
-    
+
     return event?.args?.relationshipId.toNumber();
   }
 });
