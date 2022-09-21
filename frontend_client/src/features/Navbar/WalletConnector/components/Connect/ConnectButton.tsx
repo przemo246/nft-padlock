95ccf07b8d230c67ca4eb0514772a9005404fe01
przemo246 <przemo246@gmail.com>
2022-09-21 14:13:50 +0200

    feat(ConnectButton): implement button variants

--- frontend_client/src/features/Navbar/WalletConnector/components/Connect/ConnectButton.tsx
+++ frontend_client/src/features/Navbar/WalletConnector/components/Connect/ConnectButton.tsx
@@ -1,16 +1,23 @@
 interface ConnectButtonProps {
   onClick: () => void;
   children: React.ReactNode;
+  variant?: "primary" | "secondary";
 }
 
 export const ConnectButton: React.FunctionComponent<ConnectButtonProps> = ({
   onClick,
-  children
+  children,
+  variant = "primary"
 }) => {
+  const styles =
+    variant === "primary"
+      ? "shadow-[5px_5px_0px_#F55D3E] hover:shadow-[2px_2px_0px_#F55D3E] transition-shadow bg-neutral-900"
+      : "bg-red-600";
+
   return (
     <button
       type="button"
-      className="inline-flex items-center rounded-[30px] border border-transparent transition-shadow bg-neutral-900 px-10 py-2 text-sm font-medium text-white shadow-[5px_5px_0px_#F24333] hover:shadow-[2px_2px_0px_#F24333]"
+      className={`inline-flex items-center rounded-[30px] border border-transparent px-10 py-2.5 text-sm font-medium text-white ${styles}`}
       onClick={() => onClick()}
     >
       {children}
