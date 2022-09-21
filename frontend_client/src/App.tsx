49223cefe43c188a47af3050e6e80b909c0a5870
przemo246 <przemo246@gmail.com>
2022-09-21 14:12:59 +0200

    feat(App): implement routing

--- frontend_client/src/App.tsx
+++ frontend_client/src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { DashboardLayout } from "./layout/DashboardLayout";
+import { Padlock } from "./pages/Padlock";
+import { HeroLayout } from "./layout/HeroLayout";
+import { Hero } from "./features/Hero/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<HeroLayout />}>
+        <Route index element={<Hero />} />
+      </Route>
+      <Route element={<DashboardLayout />}>
+        <Route path="/padlock" element={<Padlock />} />
+      </Route>
+    </Routes>
   );
 }
 
