4e27bf345963b856a9d80e09e99528f007da4de2
przemo246 <przemo246@gmail.com>
2022-09-21 09:43:26 +0200

    feat(Home): use Hero component

--- frontend_client/src/pages/Home.tsx
+++ frontend_client/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { PadLockControls } from "../features/PadLockControls/PadLockControls.component";
+import { Hero } from "../features/Hero/Hero";
 import { DefaultLayout } from "../layout/DefaultLayout";
 
 export default () => {
   return (
     <div className="App">
       <DefaultLayout>
-        <PadLockControls />
+        <Hero />
       </DefaultLayout>
     </div>
   );
