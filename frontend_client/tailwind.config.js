b19d170cf83bf1a0bd71d41729643006f0393d3e
przemo246 <przemo246@gmail.com>
2022-09-21 09:21:54 +0200

    chore(tailwind.config): add font family and colors

--- frontend_client/tailwind.config.js
+++ frontend_client/tailwind.config.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: "Rubik, sans-serif"
+      },
+      colors: {
+        red: {
+          600: "#F24333"
+        },
+        violet: {
+          300: "#CBC7FE"
+        }
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
