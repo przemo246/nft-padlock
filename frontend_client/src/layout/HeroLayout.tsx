4d741608538fb1caffc87e53450074e43e9a1d54
przemo246 <przemo246@gmail.com>
2022-09-21 14:15:28 +0200

    feat(HeroLayout): add Outlet component

--- frontend_client/src/layout/HeroLayout.tsx
+++ frontend_client/src/layout/HeroLayout.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from "../features/Navbar/Navbar.component";
+import { Outlet } from "react-router-dom";
 
-export const HeroLayout = ({ children }: React.PropsWithChildren<{}>) => {
+export const HeroLayout = () => {
   return (
-    <div className="bg-gradient-to-br from-violet-100 via-white to-amber-50">
-      <div className="max-w-screen-2xl my-0 mx-auto flex flex-col items-stretch h-screen">
+    <div className="h-screen bg-gradient-to-br from-violet-100 via-white to-amber-50">
+      <div className="h-full max-w-screen-2xl my-0 mx-auto flex flex-col items-stretch">
         <Navbar />
-        <main className="mx-24 h-full flex items-center justify-between">
-          {children}
+        <main className="h-full mx-24 flex items-center justify-between">
+          <Outlet />
         </main>
       </div>
     </div>
