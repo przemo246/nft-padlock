d03fde1148e1c743ab84b46c990a4c59ece89bb3
Seweryn Kras <seweryn.kras@10clouds.com>
2022-09-25 20:02:37 +0200

    remove unused code

--- frontend_client/src/features/Navbar/WethConverter/OpenModalBtn/OpenModalBtn.component.tsx
+++ frontend_client/src/features/Navbar/WethConverter/OpenModalBtn/OpenModalBtn.component.tsx
@@ -12,27 +12,5 @@ export const OpenModalBtn: React.FC<{ onClick: () => void }> = ({
       <ArrowPathRoundedSquareIcon className="h-8 w-8 text-black" />{" "}
       <span className="font-bold ml-1">Swap WETH</span>
     </button>
-    // <button
-    //   type="button"
-    //   className="px-6
-    //   py-2.5
-    //   bg-blue-600
-    //   text-white
-    //   font-medium
-    //   text-xs
-    //   leading-tight
-    //   uppercase
-    //   rounded
-    //   shadow-md
-    //   hover:bg-blue-700 hover:shadow-lg
-    //   focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-    //   active:bg-blue-800 active:shadow-lg
-    //   transition
-    //   duration-150
-    //   ease-in-out"
-    //   onClick={onClick}
-    // >
-    //   Launch demo modal
-    // </button>
   );
 };
